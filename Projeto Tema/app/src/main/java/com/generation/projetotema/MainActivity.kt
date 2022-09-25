10ecdd215b3a8c8f0300c214ba34275929a61c04
Giovanna Zamprônio <giovannazampronio@outlook.com>
2022-09-25 19:20:18 -0300

    21/09/22

---
+++ Projeto Tema/app/src/main/java/com/generation/projetotema/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.generation.projetotema
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
